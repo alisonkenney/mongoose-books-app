c64141a648995c4177fedcebc0c2a30f911be9e3
madeleinemarie <maddy.vandenheuvel@gmail.com>
2016-07-06 15:51:07 -0600

    Almost completes sprint 1

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ var express = require('express'),
 // generate a new express app and call it 'app'
 var app = express();
 
+//import modules
+var db = require('./models');
+
 // serve static files in public
 app.use(express.static('public'));
 
@@ -24,32 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 ////////////////////
 //  DATA
 ///////////////////
-
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-
 
 
 
@@ -70,20 +47,26 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, books){
+    if (err) { return console.log("index error: " + err); }
+    res.json(books);
+  });
 });
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
-  // find one book by its id
-  console.log('books show', req.params);
-  for(var i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
-    }
-  }
+  db.Book.find({_:id = req.params.id}, function(err, books){
+    if (err) { return console.log("index error: " + err);}
+    res.json(books);
+  });
+  //original:
+  //console.log('books show', req.params);
+  //for(var i=0; i < books.length; i++) {
+    //if (books[i]._id === req.params.id) {
+      //res.json(books[i]);
+      //break; // we found the right book, we can stop searching
+    //}
+  //}
 });
 
 // create new book
